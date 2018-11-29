a07471bdcb90904b4c6d72e8421a802c495f3bcc
vinodhveeraraghavan <45471872+vinodhveeraraghavan@users.noreply.github.com>
2018-11-30 10:57:49 +1100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Telstra CI/CD</h1>
-                <h2>Congratulations</h2>
+                <h2>Congratulations Vinodh</h2>
                 <p>Your AWS application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
